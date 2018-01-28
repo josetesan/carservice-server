f062a8a84201655c0a4f953623bbfaa1b44e61e6
Jose Luis Sanchez <muallin@gmail.com>
2018-01-28 18:46:42 +0100

    added cors

--- src/main/java/com/josetesan/poc/CarServer/beans/CarRepository.java
+++ src/main/java/com/josetesan/poc/CarServer/beans/CarRepository.java
@@ -2,7 +2,9 @@ package com.josetesan.poc.CarServer.beans;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
--- src/main/java/com/josetesan/poc/CarServer/controllers/CarController.java
+++ src/main/java/com/josetesan/poc/CarServer/controllers/CarController.java
@@ -2,6 +2,7 @@ package com.josetesan.poc.CarServer.controllers;
 
 import com.josetesan.poc.CarServer.beans.Car;
 import com.josetesan.poc.CarServer.beans.CarRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class CarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return carRepository.findAll().stream()
                 .filter(this::isCool)
